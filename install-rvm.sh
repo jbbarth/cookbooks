f1dc614fc5c3cc4006f77638332aafed180cdaaf
jbbarth <jeanbaptiste.barth@gmail.com>
2011-01-18 18:11:57 +0100

    Added an install-rvm.sh script (should be run as root before any user definition and before install-chef.sh

---
+++ install-rvm.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ "$(id -u)" != 0 ]; then
+  echo "Vous devez lancer ce script avec l'utilisateur root!" >&2
+  exit 1
+fi
+
+aptitude install -y build-essential bison openssl libreadline5 libreadline5-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libcurl4-openssl-dev
+bash < <( curl -L http://bit.ly/rvm-install-system-wide )
+echo -e "\n# RVM\nsource /usr/local/lib/rvm" >> /root/.bashrc
+source /root/.bashrc
+sed -i 's/^.*PS1.*return$/if [[ -n "$PS1" ]]; then/' /etc/skel/.bashrc
+echo -e "\nfi #endof Interactive test\n# RVM\nsource /usr/local/lib/rvm" >> /etc/skel/.bashrc
