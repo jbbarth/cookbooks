204c9fb7f62648ec8114115a03ba54beb53e0cb2
jbbarth <jeanbaptiste.barth@gmail.com>
2011-01-21 09:26:23 +0100

    Added basic ssh recipe to handle /etc/ssh/sshd_config

---
+++ cookbooks/ssh/recipes/default.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook Name:: ssh
+# Recipe:: default
+#
+# Copyright 2011, Jean-Baptiste Barth
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+service "ssh" do
+  supports :restart => true, :reload => true
+  action :enable
+end
+
+template "/etc/ssh/sshd_config" do
+  source "sshd_config.erb"
+  mode 0644
+  owner "root"
+  group "root"
+  notifies :restart, resources(:service => "ssh")
+end
+
+service "ssh" do
+  action :start
+end
