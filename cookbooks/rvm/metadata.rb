45bfbacc83214344feb11b79d982dbccd19c54d0
jbbarth <jeanbaptiste.barth@gmail.com>
2011-01-26 16:17:17 +0100

    Added rvm cookbook ; handles only rvm system-wide install for the moment, same as http://cookbooks.opscode.com/cookbooks/rvm, but more to come

---
+++ cookbooks/rvm/metadata.rb
@@ -0,0 +1,10 @@
+maintainer       "Jean-Baptiste Barth"
+maintainer_email "jeanbaptiste.barth@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures rvm"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.textile'))
+version          "0.1"
+
+%w(debian ubuntu).each do |os|
+  supports os
+end
---
+++ cookbooks/rvm/recipes/default.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: rvm
+# Recipe:: default
+#
+# Copyright 2011, Jean-Baptiste Barth
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "build-essential"
+
+packages = %w(openssl libreadline5 libreadline5-dev curl git-core file sqlite3
+              zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev 
+              libxml2-dev libxslt1-dev libcurl4-openssl-dev)
+
+packages.each do |package|
+  apt_package package
+end
+
+group "rvm" do
+  gid 2001
+end
+
+execute "install-rvm-system-wide" do
+  command "bash < <( curl -L http://bit.ly/rvm-install-system-wide )"
+  not_if "test -s /usr/local/lib/rvm"
+end
+
+file "/etc/profile.d/rvm.sh" do
+  owner "root"
+  group "root"
+  mode  "644"
+  content %(# Dropped off by Chef !\n# Loads RVM (Ruby Version Manager)\nsource /usr/local/lib/rvm\n)
+end
