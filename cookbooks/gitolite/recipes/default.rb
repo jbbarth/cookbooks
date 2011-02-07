c183227a7b1fec8cfb56755a215800b5b16d391f
jbbarth <jeanbaptiste.barth@gmail.com>
2011-02-07 13:41:51 +0100

    Use gitolite master branch since pu seems to be broken

--- cookbooks/gitolite/recipes/default.rb
+++ cookbooks/gitolite/recipes/default.rb
@@ -25,9 +25,11 @@ bash "install-gitolite" do
   code <<-EOH
     git clone git://github.com/sitaramc/gitolite gitolite-source
     cd gitolite-source
-    git checkout -t origin/pu
+    #git checkout -t origin/pu
+    git checkout master
     mkdir -p /usr/local/share/gitolite/conf /usr/local/share/gitolite/hooks
     src/gl-system-install /usr/local/bin /usr/local/share/gitolite/conf /usr/local/share/gitolite/hooks
+    rm -rf gitolite-source
   EOH
   not_if { File.directory?("/usr/local/share/gitolite") }
 end
