7447291b4177c1bde2c5d121d58f430cfbe25bb0
jbbarth <jeanbaptiste.barth@gmail.com>
2011-02-08 07:31:53 +0100

    Do not hide cookbooks update anymore

--- run-chef-solo.sh
+++ run-chef-solo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source "/usr/local/rvm/scripts/rvm" || exit 1
-cd /var/chef; git pull origin master >/dev/null 2>&1
+cd /var/chef; git pull origin master 2>&1 | sed 's/^/COOKBOOKS UPDATE: /
 rvm 1.9.2@chef
 chef-solo
