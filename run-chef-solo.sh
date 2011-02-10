fbce0e5189b421597dce3b8ed321a2ba5fccfd00
jbbarth <jeanbaptiste.barth@gmail.com>
2011-02-10 06:57:36 +0100

    Fixed oopsie in run-chef-solo.sh

--- run-chef-solo.sh
+++ run-chef-solo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source "/usr/local/rvm/scripts/rvm" || exit 1
-cd /var/chef; git pull origin master 2>&1 | sed 's/^/COOKBOOKS UPDATE: /
+cd /var/chef; git pull origin master 2>&1 | sed 's/^/COOKBOOKS UPDATE: /'
 rvm 1.9.2@chef
 chef-solo
