fcace5a2f96a5ffc6c447f339c3f41c4d41225da
jbbarth <jeanbaptiste.barth@gmail.com>
2011-08-20 03:35:55 +0200

    Refined the way we run chef: do not pull updates if not necessary, and remove useless comments in chef output

--- run-chef-solo.sh
+++ run-chef-solo.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 source "/usr/local/rvm/scripts/rvm" || exit 1
-cd /var/chef; git pull origin master 2>&1 | sed 's/^/COOKBOOKS UPDATE: /'
+cd /var/chef
+localrev=$(git rev-parse HEAD)
+remoterev=$(git ls-remote origin master|awk '{print $1}')
+if [ "$remoterev" != "$localrev" ]; then
+  git pull origin master 2>&1 | sed 's/^/COOKBOOKS UPDATE: /'
+fi
 rvm 1.9.2@chef
-chef-solo $*
+chef-solo $* |grep -v -e "INFO: Processing" -e "INFO: Run List" -e "INFO: Setting the run list"
