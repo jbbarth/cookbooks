82f095944632b671fa8ab5e449cfa295fa05da84
jbbarth <jeanbaptiste.barth@gmail.com>
2011-08-20 03:53:00 +0200

    Hide useless log message in run-chef-solo.sh

--- run-chef-solo.sh
+++ run-chef-solo.sh
@@ -7,4 +7,4 @@ if [ "$remoterev" != "$localrev" ]; then
   git pull origin master 2>&1 | sed 's/^/COOKBOOKS UPDATE: /'
 fi
 rvm 1.9.2@chef
-chef-solo $* |grep -v -e "INFO: Processing" -e "INFO: Run List" -e "INFO: Setting the run list"
+chef-solo $* |grep -v -e "INFO: Processing" -e "INFO: Run List" -e "INFO: Setting the run_list"
