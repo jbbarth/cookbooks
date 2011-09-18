c667c0283ad567d85444c648afac54e89c4a0dab
jbbarth <jeanbaptiste.barth@gmail.com>
2011-09-18 14:14:17 +0200

    Filter out other Gem.source_index deprecation warnings

--- run-chef-solo.sh
+++ run-chef-solo.sh
@@ -5,5 +5,5 @@ if ! git show $(git ls-remote origin master|awk '{print $1}') >/dev/null; then
   git pull origin master 2>&1 | sed 's/^/COOKBOOKS UPDATE: /'
 fi
 rvm 1.9.2@chef
-chef-solo $* 2>&1|fgrep -v -e "INFO: Processing" -e "INFO: Run List" -e "INFO: Setting the run_list" \
-                           -e "Gem::SourceIndex#search" -e "Gem.source_index"
+chef-solo $* 2>&1|grep -v -e "INFO: Processing" -e "INFO: Run List" -e "INFO: Setting the run_list" \
+                          -e "^Gem::SourceIndex" -e "^NOTE: Gem::SourceIndex" -e "Gem.source_index"
