744893c71009dd5c6008bde26533ebb7d1df47eb
jbbarth <jeanbaptiste.barth@gmail.com>
2011-09-18 11:08:19 +0200

    Send chef error messages to stdin so that they're filtered. TODO: refine it a bit...

--- run-chef-solo.sh
+++ run-chef-solo.sh
@@ -3,9 +3,11 @@ source "/usr/local/rvm/scripts/rvm" || exit 1
 cd /var/chef
 localrev=$(git rev-parse HEAD)
 remoterev=$(git ls-remote origin master|awk '{print $1}')
+echo $remoterev
+echo $localrev
 if [ "$remoterev" != "$localrev" ]; then
   git pull origin master 2>&1 | sed 's/^/COOKBOOKS UPDATE: /'
 fi
 rvm 1.9.2@chef
-chef-solo $* |fgrep -v -e "INFO: Processing" -e "INFO: Run List" -e "INFO: Setting the run_list" \
-                       -e "Gem::SourceIndex#search"
+chef-solo $* 2>&1|fgrep -v -e "INFO: Processing" -e "INFO: Run List" -e "INFO: Setting the run_list" \
+                           -e "Gem::SourceIndex#search"
