892092d5551116a79323e43bae9d7a49e13c52f6
jbbarth <jeanbaptiste.barth@gmail.com>
2011-09-18 11:10:32 +0200

    Improved the way we detect the need for a git update ; if the remote revision is in the history, don't git pull

--- run-chef-solo.sh
+++ run-chef-solo.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 source "/usr/local/rvm/scripts/rvm" || exit 1
 cd /var/chef
-localrev=$(git rev-parse HEAD)
-remoterev=$(git ls-remote origin master|awk '{print $1}')
-echo $remoterev
-echo $localrev
-if [ "$remoterev" != "$localrev" ]; then
+if ! git show $(git ls-remote origin master|awk '{print $1}') >/dev/null; then
   git pull origin master 2>&1 | sed 's/^/COOKBOOKS UPDATE: /'
 fi
 rvm 1.9.2@chef
