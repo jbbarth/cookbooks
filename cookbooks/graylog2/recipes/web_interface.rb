ff0279a6cb48cea2b9245a794a983fe09266f960
jbbarth <jeanbaptiste.barth@gmail.com>
2012-01-28 16:43:30 +0100

    graylog2: avoid unnecessary mail notifications

--- cookbooks/graylog2/recipes/web_interface.rb
+++ cookbooks/graylog2/recipes/web_interface.rb
@@ -103,13 +103,13 @@ end
 cron "Graylog2 send stream alarms" do
   minute node[:graylog2][:stream_alarms_cron_minute]
   action node[:graylog2][:send_stream_alarms] ? :create : :delete
-  command "bash -c 'source /usr/local/rvm/scripts/rvm && cd #{node[:graylog2][:basedir]}/web && RAILS_ENV=production bundle exec rake streamalarms:send'"
+  command "bash -c 'source /usr/local/rvm/scripts/rvm && cd #{node[:graylog2][:basedir]}/web && RAILS_ENV=production bundle exec rake streamalarms:send >/dev/null'"
 end
 
 cron "Graylog2 send stream subscriptions" do
   minute node[:graylog2][:stream_subscriptions_cron_minute]
   action node[:graylog2][:send_stream_subscriptions] ? :create : :delete
-  command "bash -c 'source /usr/local/rvm/scripts/rvm && cd #{node[:graylog2][:basedir]}/web && RAILS_ENV=production bundle exec rake subscriptions:send'"
+  command "bash -c 'source /usr/local/rvm/scripts/rvm && cd #{node[:graylog2][:basedir]}/web && RAILS_ENV=production bundle exec rake subscriptions:send >/dev/null'"
 end
 
 #startup script
