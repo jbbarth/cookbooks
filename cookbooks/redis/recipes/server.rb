104e2b7ebd428977738408076527104f8d16d47d
jbbarth <jeanbaptiste.barth@gmail.com>
2012-01-29 01:18:34 +0100

    redis: don't run installation again if redis is already installed in the right version

--- cookbooks/redis/recipes/server.rb
+++ cookbooks/redis/recipes/server.rb
@@ -1,80 +1,84 @@
 include_recipe "redis::client" 
 
-remote_file "/tmp/redis-#{node[:redis][:version]}.tar.gz" do
-  source "http://redis.googlecode.com/files/redis-#{node[:redis][:version]}.tar.gz"
-end
+version_installed=%x(which redis-cli >/dev/null 2>&1 && redis-cli info|grep redis_version|cut -d":" -f 2).chomp
 
-execute "tar xvfz /tmp/redis-#{node[:redis][:version]}.tar.gz" do
-  cwd "/tmp"
-end
+if version_installed != node[:redis][:version]
+  remote_file "/tmp/redis-#{node[:redis][:version]}.tar.gz" do
+    source "http://redis.googlecode.com/files/redis-#{node[:redis][:version]}.tar.gz"
+  end
 
-execute "make" do
-  cwd "/tmp/redis-#{node[:redis][:version]}"
-end
+  execute "tar xvfz /tmp/redis-#{node[:redis][:version]}.tar.gz" do
+    cwd "/tmp"
+  end
 
-user "redis" do
-  shell "/bin/zsh"
-  action :create
-end
+  execute "make" do
+    cwd "/tmp/redis-#{node[:redis][:version]}"
+  end
 
-directory ::File.dirname(node[:redis][:swapfile]) do
-  action :create
-  recursive true
-  owner node[:redis][:user]
-  group node[:redis][:user]
-  mode '0755'
-end
+  user "redis" do
+    shell "/bin/zsh"
+    action :create
+  end
 
-directory node[:redis][:datadir] do
-  action :create
-  recursive true
-  owner node[:redis][:user]
-  group 'users'
-  mode '0755'
-end
+  directory ::File.dirname(node[:redis][:swapfile]) do
+    action :create
+    recursive true
+    owner node[:redis][:user]
+    group node[:redis][:user]
+    mode '0755'
+  end
 
-directory File.dirname(node[:redis][:log_file]) do
-  action :create
-  owner node[:redis][:user]
-  group 'root'
-  mode '0755'
-end
+  directory node[:redis][:datadir] do
+    action :create
+    recursive true
+    owner node[:redis][:user]
+    group 'users'
+    mode '0755'
+  end
 
-enclosed_node = node
-ruby_block "Install binaries" do
-  block do
-    %w{redis-server redis-cli redis-benchmark redis-check-aof redis-check-dump}.each do |binary|
-      FileUtils.install "/tmp/redis-#{enclosed_node[:redis][:version]}/src/#{binary}",
-                        "#{enclosed_node[:redis][:prefix]}/bin", :mode => 0755
-      FileUtils.chown enclosed_node[:redis][:user], 'users', "#{enclosed_node[:redis][:prefix]}/bin/#{binary}"
+  directory File.dirname(node[:redis][:log_file]) do
+    action :create
+    owner node[:redis][:user]
+    group 'root'
+    mode '0755'
+  end
+
+  enclosed_node = node
+  ruby_block "Install binaries" do
+    block do
+      %w{redis-server redis-cli redis-benchmark redis-check-aof redis-check-dump}.each do |binary|
+        FileUtils.install "/tmp/redis-#{enclosed_node[:redis][:version]}/src/#{binary}",
+                          "#{enclosed_node[:redis][:prefix]}/bin", :mode => 0755
+        FileUtils.chown enclosed_node[:redis][:user], 'users', "#{enclosed_node[:redis][:prefix]}/bin/#{binary}"
+      end
     end
   end
+
+  template "/etc/init.d/redis-server" do
+    source "redis.init.erb"
+    owner "root"
+    group "root"
+    mode "0755"
+  end
+
+  include_recipe "redis::service"
+
+  service "redis-server" do
+    action :enable
+  end
+
+  execute "ensure correct permissions" do
+    command "chown -R #{node[:redis][:user]} #{node[:redis][:datadir]} #{node[:redis][:log_file]}"
+    ignore_failure true # newley created dirs
+  end
+
+  template "/etc/redis.conf" do
+    source "redis.conf.erb"
+    owner "root"
+    group "root"
+    mode "0644"
+    notifies :restart, resources(:service => "redis-server"), :immediately
+  end
+
+  include_recipe 'redis::backup'
 end
-
-template "/etc/init.d/redis-server" do
-  source "redis.init.erb"
-  owner "root"
-  group "root"
-  mode "0755"
-end
-
-include_recipe "redis::service"
-
-service "redis-server" do
-  action :enable
-end
-
-execute "ensure correct permissions" do
-  command "chown -R #{node[:redis][:user]} #{node[:redis][:datadir]} #{node[:redis][:log_file]}"
-  ignore_failure true # newley created dirs
-end
-
-template "/etc/redis.conf" do
-  source "redis.conf.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  notifies :restart, resources(:service => "redis-server"), :immediately
-end
-
-include_recipe 'redis::backup'
