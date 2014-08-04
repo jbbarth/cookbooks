1753b1fe6763774d651b9015adf5074ecbf9d6d9
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-08-04 11:39:59 +0200

    Switch to ruby 1.9.3 in ./run-chef-solo.sh

--- run-chef-solo.sh
+++ run-chef-solo.sh
@@ -4,6 +4,6 @@ cd /var/chef
 if ! git show $(git ls-remote origin master|awk '{print $1}') >/dev/null; then
   git pull origin master 2>&1 | sed 's/^/COOKBOOKS UPDATE: /'
 fi
-rvm 1.9.2@chef
+rvm 1.9.3@chef
 chef-solo $* 2>&1|grep -v -e "INFO: Processing" -e "INFO: Run List" -e "INFO: Setting the run_list" \
                           -e "^Gem::SourceIndex" -e "^NOTE: Gem::SourceIndex" -e "Gem.source_index"
