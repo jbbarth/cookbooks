6f841d41f5dc839f8c4718770085730cc58542b4
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-08-04 11:21:49 +0200

    Simplify prerequisites for rvm installation because now rvm know how to install needed packages

--- install-rvm.sh
+++ install-rvm.sh
@@ -12,12 +12,8 @@ if [ -s "/usr/local/rvm" ]; then
 fi
 
 if test -e /etc/debian_version; then
-  # package dependencies
-  apt-get install -y build-essential bison openssl libreadline5 libreadline5-dev \
-                     curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev \
-                     libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev \
-                     autoconf libcurl4-openssl-dev file
-
+  # needed to fetch rvm
+  apt-get install curl git-core
   # rvm group
   groupadd -f -g 2001 rvm
 fi
